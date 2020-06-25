46013b7eb841ebdf8facdfcf0a58497ff5304794
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-06-25 22:54:09 +0530

    Format code

--- BlogApp/__init__.py
+++ BlogApp/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
-from .accounts.urls import accounts
-from .blogs.urls import blogs
-
 
 def create_app():
 
     app = Flask(__name__)
+    app.add_url_rule('/', endpoint='index')
 
+    from .accounts.urls import accounts
+    from .blogs.urls import blogs
     app.register_blueprint(accounts)
     app.register_blueprint(blogs)
 
