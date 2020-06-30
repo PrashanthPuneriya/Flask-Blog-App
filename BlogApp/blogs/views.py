3623107e405c60f8f5397fa1c75a5003fee688fe
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-06-30 23:25:11 +0530

    Complete PostAllView view

--- BlogApp/blogs/views.py
+++ BlogApp/blogs/views.py
@@ -1,10 +1,49 @@
 from flask.views import MethodView
-# from 
+from flask import make_response, jsonify
+from .. import db
+from flask import json
+
 
 class PostAllView(MethodView):
     def get(self):
-        
-        pass
+        connection = db.get_db()
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT title, content, users.email FROM blogs, users;"
+        )
+
+        """
+            cursor.execute(
+                "INSERT INTO blogs VALUES (%s, %s, %s, %s)",
+                (2, "user1-title2", "user1-content2", 1)
+            )
+            connection.commit()
+
+            print(cursor.description) # tuple of tuples
+            print(posts) # list of tuples
+
+            for col in cursor.description:
+                print(col[0]) # Column names   
+        """
+
+        rows = cursor.fetchall()
+        column_names = [col[0] for col in cursor.description]
+        # posts = [dict(zip(column_names, row)) for row in rows]
+        posts = []
+        for row in rows:
+            obj = dict(zip(column_names, row))
+            posts.append(obj)
+
+        print(type(posts))
+
+        response = make_response(jsonify(posts), 200)
+        # response = response_class(
+        #     response=json.dumps(posts),
+        #     status=200,
+        #     mimetype='application/json'
+        # )
+
+        return response
 
     def delete(self):
         # Delete all the posts
--- BlogApp/schema.sql
+++ BlogApp/schema.sql
@@ -1,12 +1,14 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS blogs;
+
 CREATE TABLE users(
     id serial PRIMARY KEY,
     first_name VARCHAR (255) NOT NULL,
     last_name VARCHAR (255),
-    email VARCHAR (255),
-    password VARCHAR (255)
+    email VARCHAR (255) NOT NULL UNIQUE,
+    password VARCHAR (255) NOT NULL
 );
+
 CREATE TABLE blogs(
     id serial PRIMARY KEY,
     title VARCHAR (255) NOT NULL,
