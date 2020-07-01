c89b10a8c38a1291878a4568cc975f6649233092
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-01 15:37:50 +0530

    Implement PostUserView

--- BlogApp/blogs/urls.py
+++ BlogApp/blogs/urls.py
@@ -12,6 +12,6 @@ blogs = Blueprint(
     url_prefix='/'
 )
 
-blogs.add_url_rule('all-posts/', view_func=PostAllView.as_view('post-all'))
 blogs.add_url_rule('<int:post_id>/', view_func=PostDetailView.as_view('post-detail'))
+blogs.add_url_rule('all-posts/', view_func=PostAllView.as_view('post-all'))
 blogs.add_url_rule('user-posts/', view_func=PostUserView.as_view('post-user'))
--- BlogApp/blogs/views.py
+++ BlogApp/blogs/views.py
@@ -1,11 +1,12 @@
 from flask.views import MethodView
-from flask import make_response
+from flask import make_response, request
 from .. import db
 import json
 
 
 class PostAllView(MethodView):
     def get(self):
+        # Get all posts
         connection = db.get_db()
         cursor = connection.cursor()
         cursor.execute(
@@ -14,8 +15,8 @@ class PostAllView(MethodView):
 
         """
         cursor.execute(
-            "INSERT INTO blogs VALUES (%s, %s, %s, %s)",
-            (2, "user1-title2", "user1-content2", 1)
+            "INSERT INTO blogs (title, content, user_id) VALUES (%s, %s, %s)",
+            ("user1-title2", "user1-content2", 1)
         )
         connection.commit()
 
@@ -47,29 +48,78 @@ class PostAllView(MethodView):
         return response
 
     def delete(self):
+        # Delete all posts
         connection = db.get_db()
         cursor = connection.cursor()
         cursor.execute(
             "DELETE FROM blogs;"
         )
         connection.commit()
-        data = {'Success': 'Deleted all posts successfully'}
-        response = make_response(data, 200)
-        return response
+        return ({'Success': 'Deleted all the user posts successfully'})
 
 
 class PostUserView(MethodView):
+    # User ID must be obtained via auth for the following methods. For now included a dummy user_id
     def get(self):
-        # Lists all the posts of the user
-        pass
+        # List all the posts of the user
+
+        user = 1
+
+        connection = db.get_db()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "SELECT title, content FROM blogs WHERE blogs.user_id = %s;", str(user)
+        )
+
+        rows = cursor.fetchall()
+        print(rows)
+        print(cursor.description)
+        column_names = [col[0] for col in cursor.description]
+        posts = [dict(zip(column_names, row)) for row in rows]
+
+        response = make_response(json.dumps(posts), 200)
+        return response
 
     def post(self):
         # Create a post under the user
-        pass
+
+        user = 1
+
+        connection = db.get_db()
+        cursor = connection.cursor()
+
+        data = request.get_json()
+        post_title = data['title']
+        post_content = data['content']
+
+        cursor.execute(
+            "INSERT INTO blogs (title, content, user_id) VALUES (%s, %s, %s);",
+            (post_title, post_content, user)
+        )
+
+        cursor.execute(
+            "SELECT email FROM users WHERE id = %s;", str(user)
+        )
+
+        user_email = cursor.fetchone()
+
+        connection.commit()
+
+        return ({'Success': 'Post saved successfully', 'title': post_title, 'content': post_content, 'user': user_email[0]})
 
     def delete(self):
         # Delete all the user posts
-        pass
+
+        user = 1
+
+        connection = db.get_db()
+        cursor = connection.cursor()
+        cursor.execute(
+            "DELETE FROM blogs WHERE user_id = %s;", str(user)
+        )
+        connection.commit()
+        return ({'Success': 'Deleted all the user posts successfully'})
 
 
 class PostDetailView(MethodView):
